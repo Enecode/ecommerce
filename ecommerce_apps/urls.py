8d66c5f3aa0d7be9dafe8cdd93ad4fecef6190f8
Isah Jacob <isahjacob0@gmail.com>
2023-01-18 14:23:23 +0100

    deploy

---
+++ ecommerce_apps/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from ecommerce_apps import views
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', views.index, name="index"),
+    path('category_detail/', views.category_detail, name="category_detail"),
+    path('product_list/', views.product_list, name="product_list"),
+]
---
+++ ecommerce_apps/views.py
@@ -0,0 +1,93 @@
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from ecommerce_apps.forms import CheckoutForm
+from .models import Product, Order, Customer, Collection, CartItem
+import requests
+
+
+def index(request):
+    products = Product.objects.all()
+
+    response = requests.get('https://picsum.photos/picsum/300/300')
+    images = response.json()
+    # images = HttpResponse('image')
+    return render(request, 'ecommerce_app/index.html', {'products': products, 'images': images})
+
+
+def product_list(request):
+    products = Product.objects.all()
+    """
+    Displays a list of all products.
+    """
+    return render(request, 'ecommerce_app/product_list.html', {'products': products})
+
+
+def product_detail(request, pk):
+    """
+    Displays the details of a specific product.
+    """
+    product = Product.objects.get(pk=pk)
+    return render(request, 'ecommerce_app/product_detail.html', {'product': product})
+
+
+def category_detail(request):
+    """
+    Displays the details of a category.
+    """
+    categories = Collection.objects.all()
+    # categories = get_object_or_404(Collection)
+    return render(request, 'ecommerce_app/category_detail.html', {'categories': categories})
+
+
+def add_to_cart(request, pk):
+    """
+    Adds a product to the cart.
+    """
+    product = Product.objects.get(pk=pk)
+    # add product to the cart
+    return redirect('cart')
+
+
+def cart(request, product, quantity):
+    """
+    Displays the contents of the cart.
+    """
+    cart_items = CartItem()
+    cart_items.add_item(product, quantity)
+    cart_items.remove_item(product)
+    cart_items.clear()
+    total_price = cart_items.total_price()
+    return render(request, 'cart.html', {'cart_items': cart_items, 'total_price': total_price})
+
+
+def checkout(request, quantity, product):
+    """
+    Processes the order and displays the confirmation page.
+    """
+    cart_items = CartItem()
+    cart_items.add_item(product, quantity)
+    cart_items.remove_item(product)
+    cart_items.clear()
+    total_price = cart
+
+    if request.method == 'POST':
+        form = CheckoutForm(request.POST)
+        if form.is_valid():
+            # create an Order and Customer objects
+            customer, _ = Customer.objects.get_or_create(
+                name=form.cleaned_data['name'],
+                email=form.cleaned_data['email'],
+                phone=form.cleaned_data['phone'],
+                shipping_addresses=form.cleaned_data['shipping_addresses'],
+            )
+            order = Order.objects.create(
+                customer=customer,
+                total_price=form.cleaned_data['total_price'],
+            )
+            order.items.set(form.cleaned_data['items'])
+            # clear the cart
+            cart_items.clear()
+            return render(request, 'confirmation.html', {'order': order})
+    else:
+        form = CheckoutForm()
+    return render(request, 'checkout.html', {'form': form})
