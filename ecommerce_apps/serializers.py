e748120a2ad741626474101255b1dd67b5dacfa4
Isah Jacob <isahjacob0@gmail.com>
2023-01-21 12:32:49 +0100

    Serializer for products and collections

--- ecommerce_apps/serializers.py
+++ ecommerce_apps/serializers.py
@@ -3,26 +3,41 @@ from rest_framework import serializers
 from ecommerce_apps.models import Product, Collection
 
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ['id', 'title', 'products_count']
+
+    products_count = serializers.IntegerField()
+
+    def create(self, validated_data):
+        collection = Collection(**validated_data)
+        collection.other = 1
+        collection.save()
+        return collection
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id', 'title', 'unit_price', 'collection', 'price_with_tax']
+        fields = ['id', 'title', 'slug', 'description', 'inventory', 'unit_price', 'price_with_tax', 'collection',
+                  'price_with_tax']
 
-    # id = serializers.IntegerField()
-    # title = serializers.CharField(max_length=255)
     # price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
-    # description = serializers.HyperlinkedRelatedField()
+    # description = serializers.StringRelatedField()
     price_with_tax = serializers.SerializerMethodField(method_name='calculate_tax')
-    collection = serializers.HyperlinkedRelatedField(
-        required=Product.description,
-        queryset=Collection.objects.all(),
-        view_name='ecommerce_apps:collection_detail'
-    )
+    collection = serializers.StringRelatedField()
+
+    # collection = serializers.HyperlinkedRelatedField(
+    #     queryset=Collection.objects.all(),
+    #     view_name='ecommerce_apps:collection_detail'
+    # )
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
+
+    def create(self, validated_data):
+        product = Product(**validated_data)
+        product.other = 1
+        product.save()
+        return product
